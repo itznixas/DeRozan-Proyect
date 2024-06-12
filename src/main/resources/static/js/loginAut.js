8d43045b8cb6d4c17d0056969da9875693be6e38
Royer Fontalvo <rogerjrfontalvo@gmail.com>
2024-06-11 21:54:22 -0500

    Resolvi los conflicto del pull

--- src/main/resources/static/js/loginAut.js
+++ src/main/resources/static/js/loginAut.js
@@ -1,27 +1,31 @@
-import axios from 'axios';
+document.getElementById('login-form').addEventListener('submit', async function (e) {
+  e.preventDefault();
 
+  const email = document.getElementById('login_email').value;
+  const password = document.getElementById('login_password').value;
 
-function LoginForm({ onSignupClick, onLoginSuccess }) {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [error, setError] = useState(null);
-  
-    const handleLogin = async (e) => {
-      e.preventDefault();
-      try {
-        const response = await axios.post('/api/auth/login', {
-          email: email,
-          password: password
-        });
-  
-        if (response.status === 200) {
-          console.log('Login successful');
-          onLoginSuccess();
-        }
-  
-      } catch (err) {
-        console.error('Error during login', err);
-        setError('Login failed. Please check your credentials and try again.');
-      }
-    };
-}
+  try {
+    const response = await axios.post('/api/auth/login', {
+      email: email,
+      password: password
+    });
+
+    if (response.status === 200) {
+      new Noty({
+      type: 'success',
+      layout: 'topRight',
+      text: 'You have been logged in successfully.',
+      timeout: 3000
+    }).show();
+      // Aquí puedes redirigir al usuario o mostrar un mensaje de éxito
+    }
+  } catch (err) {
+    new Noty({
+    type: 'error',
+    layout: 'topRight',
+    text: 'Please check your credentials and try again.',
+    timeout: 3000
+  }).show();
+    // Aquí puedes mostrar un mensaje de error al usuario
+  }
+});
