65a3a74ad6b66620bc4ca880b536e215c47d633b
Royer Fontalvo <rogerjrfontalvo@gmail.com>
2024-06-20 23:35:16 -0500

    admin y user, informacion de cuenta

--- target/classes/static/js/super-admin/productos/settings.js
+++
@@ -1,6 +0,0 @@
-const editBtnSA = document.querySelector('#editSAbtn'),
-    settings = document.querySelector('.settings'),
-    editCloseBtnSA = document.querySelector('#editCloseSA');
-
-    editBtnSA.addEventListener("click", () => settings.classList.add("show"));
-    editCloseBtnSA.addEventListener("click", () => settings.classList.remove("show"));
